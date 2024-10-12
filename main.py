75864255dc894bfa3b182e3b25e16233414084ee
kchen53 <kellychen@ufl.edu>
2024-10-12 17:01:31 -0400

    Added an intro and a menu

---
+++ main.py
@@ -0,0 +1,6 @@
+from start import start
+
+start = start
+start.intro()
+start.robot()
+selection = start.menu()
---
+++ start.py
@@ -0,0 +1,33 @@
+class start:
+    
+    def intro():
+        print("Welcome to TutorMe!", end="\n\n")
+        print("My name is Logi and I'm here to help you set up your tutoring space.", end="\n")
+        print("My bot friends and I are so excited to learn from you!", end="\n\n")
+
+    def robot():
+        print("""
+             ___T_     
+            | # # |    
+            |__v__|    
+           .=[::+]=.   
+         ]=' [___] '=[ 
+             /  |      
+            _\  |_    
+        """)
+        print()
+        
+    def menu():
+        print("Please let me know how I can help!", end="\n\n")
+        print("Here are some options: ", end="\n\n")
+        print("1. Request a new tutor")
+        print("2. Return to a past tutor")
+        print("3. Remove a past tutor")
+        print("4. View all current tutors", end="\n\n")
+
+        selction = input("What's your choice? ")
+
+        return selction
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
